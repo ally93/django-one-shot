e0aefc8c5f4c2707ed7eee58741d36524bc20ca0
Ally <allygoose93@gmail.com>
2022-09-07 15:27:13 -0700

    Feature 7 complete

--- brain_two/urls.py
+++ brain_two/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-# , include
+from django.urls import include, path, reverse_lazy
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path("todos/", include("todos.urls")),
+    path("admin/", admin.site.urls),
+    path("todos/", include("todos.urls")),
+    path(
+        "",
+        RedirectView.as_view(url=reverse_lazy("todo_list_list")),
+        name="home",
+    ),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from todos.models import TodoList
 
 # Create your views here.
+class TodoListListView(ListView):
+    model = TodoList
+    template_name = "todos/list.html"
+    
