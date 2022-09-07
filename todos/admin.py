353c940eb821563488e6150fbc10159ebf20ed3d
Ally <allygoose93@gmail.com>
2022-09-07 14:00:01 -0700

    Feature 5 complete

--- todos/admin.py
+++ todos/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from todos.models import TodoList
+from todos.models import TodoList, TodoItem
 
 # Register your models here.
 class TodoListAdmin(admin.ModelAdmin):
     pass
 
 
+class TodoItemAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(TodoList, TodoListAdmin)
+admin.site.register(TodoItem, TodoItemAdmin)
--- todos/models.py
+++ todos/models.py
@@ -5,6 +5,17 @@ class TodoList(models.Model):
     name = models.CharField(max_length=100)
     created_on = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
 
-def __str__(self):
-    return self.name
+
+class TodoItem(models.Model):
+    task = models.CharField(max_length=100)
+    due_date = models.DateTimeField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    list = models.ForeignKey(
+        "TodoList", related_name="items", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.task
