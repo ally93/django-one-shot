d824c8fdb5cc826dd5a3f2ccd3d2bf88bdc2c8dd
Ally <allygoose93@gmail.com>
2022-09-07 18:02:37 -0700

    could not create in the localhost8000

--- todos/urls.py
+++ todos/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from todos.views import (
     TodoListListView,
-    TodoListDetailView
+    TodoListDetailView,
+    TodoListCreateView
 )
 
 urlpatterns = [
     path("", TodoListListView.as_view(), name="todo_list_list"),
-    path("<int:pk>/", TodoListDetailView.as_view(), name="todo_list_detail")
+    path("<int:pk>/", TodoListDetailView.as_view(), name="todo_list_detail"),
+    path("create/", TodoListCreateView.as_view(), name="todo_list_create"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,8 @@
-# from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from todos.models import TodoList
 
 # Create your views here.
@@ -12,3 +14,12 @@ class TodoListListView(ListView):
 class TodoListDetailView(DetailView):
     model = TodoList
     template_name = "todos/detail.html"
+
+
+class TodoListCreateView(CreateView):
+    model = TodoList
+    template_name = "todos/create.html"
+    fields = ["name"]
+
+    def get_success_url(self):
+        return reverse_lazy("todo_list_detail", args=[self.object.id])
